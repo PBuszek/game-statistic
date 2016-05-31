abfef9ea1677d7be6434ba892862db0ce9ccdf30
AbstractFlame <sami.gabor@gmail.com>
2016-05-31 18:15:56 +0200

    remove some content

--- part2/printing.py
+++ part2/printing.py
@@ -1,11 +1,2 @@
-from reports import *
 
-
-print(get_most_played(txt_file))
-print(sum_sold(txt_file))
-print(get_selling_avg(txt_file))
-print(count_longest_title(txt_file))
-print(get_date_avg(txt_file))
-print(get_game(txt_file, "Counter-Strike"))
-print(count_grouped_by_genre(txt_file))
-print(get_date_ordered(txt_file))
+# Printing functions
