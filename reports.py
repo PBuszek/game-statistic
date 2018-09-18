8dcb4366d39237ddc0a977effc931aafe74d4fde
PatrykMandrak <patrykmandrak123@gmail.com>
2018-09-18 23:10:43 +0200

    Part 1 without additional tasks

--- reports.py
+++ reports.py
@@ -1,2 +1,14 @@
 
 # Report functions
+
+def count_games():
+    with open('game_stat.txt') as file:
+        text_to_convert_to_list= file
+        text_to_convert_to_list=[]
+        for line in file.readlines():
+            line=line.split()
+            line[5]=line[5][:-1]
+            text_to_convert_to_list.append(line)
+    print (text_to_convert_to_list)
+
+count_games()
